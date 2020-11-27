9a959a3506ba0e80adce44df55dd93b105dcd41f
rotemdvir <66698477+rotemdvir@users.noreply.github.com>
2020-11-27 15:12:41 -0600

    Add files via upload

--- Tasks/Task4/Code_Survey4.R
+++ Tasks/Task4/Code_Survey4.R
@@ -92,7 +92,7 @@ p3 <- ggplot(MyData, aes(x = factor(approve2), group = factor(trt_covert2))) +
   scale_y_continuous(labels = scales::percent_format()) +
   xlab("") + ylab("Proportion") + 
   facet_grid(~trt_covert2, labeller = labeller(trt_covert2 = covert2.lab)) +
-  theme_bw()
+  theme_bw() + coord_flip()
 
 p3 <- p3 + theme(legend.position = 'none')
 
@@ -197,15 +197,96 @@ library(ggeffects)
 summary(m <- glm(support4 ~ factor(trt_covert3) + 
                   trust_govt + transparent_imp + gender + poli, data = MyData4, family = binomial(link = "probit")))
 
-pre <- ggpredict(m, terms = c("trt_covert3", "trust_govt [30, 60]"))
+pre <- ggpredict(m, terms = c("trt_covert3", "trust_govt [17, 30, 56, 75]"))
 p8 <- plot(pre, ci.style = "errorbar") +
   labs(
     x = "",
     y = "Probability of support",
-    title = "Predicted probability of public support",
+    title = "Predicted probability of public support for intervention policy",
     colour = "Degree of trust in government") +
   scale_x_continuous(labels = c("Overt", "Covert"), breaks = c(0, 1)) +
-  scale_colour_brewer(palette = "Set1", labels = c("Low", "High"))
+  scale_colour_brewer(palette = "Set1", labels = c("10th", "25th", "75th", "90th")) +
+  theme(legend.position = "bottom")
+
+
+# Interactions of policy type and outcomes conditions
+## Experiment 2 
+
+MyData5 <- describeBy(MyData$results2,list(MyData$trt_covert2,MyData$trt_results2), mat=TRUE,digits=2)
+
+names(MyData5)[names(MyData5) == 'group1'] = 'type'
+names(MyData5)[names(MyData5) == 'group2'] = 'outcomes'
+
+levels(MyData5$type)[levels(MyData5$type)=='0'] = 'Overt'
+levels(MyData5$type)[levels(MyData5$type)=='1'] = 'Covert'
+
+levels(MyData5$outcomes)[levels(MyData5$outcomes)=='0'] = 'Unsuccessful'
+levels(MyData5$outcomes)[levels(MyData5$outcomes)=='1'] = 'Successful'
+
+MyData5$se <- MyData5$sd/sqrt(MyData5$n)
+
+limits = aes(ymax = mean + se, ymin=mean - se)
+dodge = position_dodge(width=0.9)
+apatheme=theme_bw()+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.border=element_blank(),
+        axis.line=element_line(),
+        text=element_text(family='Times'))
+
+p9a <- ggplot(MyData5, aes(x = outcomes, y = mean, fill = type))+
+  geom_bar(stat='identity', position=dodge)+
+  geom_errorbar(limits, position=dodge, width=0.1)+
+  apatheme + scale_y_continuous(expand=c(0,0)) +
+  scale_x_discrete(labels = c("0" = "Unsuccessful", "1" = "Successful")) +
+  ylab('Mean Approval') + xlab("Policy Outcomes") + ggtitle("Experiment #2") +
+  scale_fill_manual(values = c("blue", "red"),
+                    name = "Policy Type",
+                    breaks = c(0, 1),
+                    labels = c("Overt", "Covert")) 
+  
+  
+## Experiment 3 
+
+MyData6 <- describeBy(MyData$results3,list(MyData$trt_covert3,MyData$trt_results3), mat=TRUE,digits=2)
+
+names(MyData6)[names(MyData6) == 'group1'] = 'type'
+names(MyData6)[names(MyData6) == 'group2'] = 'outcomes'
+
+levels(MyData6$type)[levels(MyData6$type)=='0'] = 'Overt'
+levels(MyData6$type)[levels(MyData6$type)=='1'] = 'Covert'
+
+levels(MyData6$outcomes)[levels(MyData6$outcomes)=='0'] = 'Unsuccessful'
+levels(MyData6$outcomes)[levels(MyData6$outcomes)=='1'] = 'Successful'
+
+MyData6$se <- MyData6$sd/sqrt(MyData6$n)
+
+limits = aes(ymax = mean + se, ymin=mean - se)
+dodge = position_dodge(width=0.9)
+apatheme=theme_bw()+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.border=element_blank(),
+        axis.line=element_line(),
+        text=element_text(family='Times'))
+
+p9b <- ggplot(MyData6, aes(x = outcomes, y = mean, fill = type))+
+  geom_bar(stat='identity', position=dodge)+
+  geom_errorbar(limits, position=dodge, width=0.1)+
+  apatheme + scale_y_continuous(expand=c(0,0)) +
+  scale_x_discrete(labels = c("0" = "Unsuccessful", "1" = "Successful")) +
+  ylab("") + xlab("Policy outcomes") + ggtitle("Experiment #3") +
+  scale_fill_manual(values = c("blue", "red"),
+                    name = "Policy Type",
+                    breaks = c(0, 1),
+                    labels = c("Overt", "Covert")) 
+
+p9 <- ggarrange(p9a, p9b, nrow = 1, ncol = 2, common.legend = T, legend = "bottom")
+p9 <- annotate_figure(p9,
+                top = text_grob("Public support for foreign intervention",
+                                color = "black",
+                                face = "bold",
+                                size = 16))
 
 
 #################### Coding for data  ####################
@@ -230,6 +311,4 @@ p8 <- plot(pre, ci.style = "errorbar") +
 # Transparency: not too important (1) ==> very important (5)
 # Trust government: 0 (hardly ever) ==> 100 (just about always).
 # Support covert action: 0=no; 1=yes.
-
-
 
